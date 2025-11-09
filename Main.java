7b1e00f12532fdd80fe4439aad2349b572406617
pravinv1998 <pravin.wargad1998@gmail.com>
2025-11-09 20:06:07 +0530

    9 oct commit

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("this is java code...");
-        System.out.println("test javaj code");
+        
     }
 }
