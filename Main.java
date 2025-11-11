e54a9364922ef037def8c693770a533c4cd635ca
pravinv1998 <pravin.wargad1998@gmail.com>
2025-11-11 22:53:16 +0530

    11 nov commit

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         System.out.println("this is java code...");
-        
+        System.out.print("1");
+        System.out.print("  Pravin");
+        System.out.println("  5295");
+
+        Main.data("pravin");
+
+    }
+
+    static void data(String name) {
+        System.out.println(name);
     }
 }
