6b252ecfe2fbc6ba2c99eac25cc7bb82f75dd347
pravinv1998 <pravin.wargad1998@gmail.com>
2025-11-11 23:33:28 +0530

    star pattern

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is java code...");
-        System.out.print("1");
-        System.out.print("  Pravin");
-        System.out.println("  5295");
+        // System.out.println("this is java code...");
+        // System.out.print("1");
+        // System.out.print(" Pravin");
+        // System.out.println(" 5295");
 
-        Main.data("pravin");
+        // Main.data("pravin");
+        //
+        // Main dataMain = new Main();
+        // dataMain.cal(5);
+
+        // print star pattern
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
     }
 
     static void data(String name) {
         System.out.println(name);
+    }
+
+    void cal(int num) {
+        System.out.println(num);
     }
 }
