93451ce9e5a15398bbec2d6b96b35a79c477f328
pravinv1998 <pravin.wargad1998@gmail.com>
2025-11-14 23:00:46 +0530

    14 Nov Commit

---
+++ Challenges.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Challenges {
+    public static void main(String[] args) {
+        Challenges chng = new Challenges();
+        chng.inpData();
+
+    }
+
+    void inpData() {
+        Scanner scnInput = new Scanner(System.in);
+
+        // try {
+        // System.out.print("Enter Your Age: ");
+        // int age = scnInput.nextInt();
+        // System.out.println("Your age is: " + age);
+        // scnInput.close();
+        // } catch (Exception e) {
+        // System.out.println("Please Enter Number!!!!");
+        // }
+    }
+}
---
+++ Java_basic.java
@@ -0,0 +1,17 @@
+public class Java_basic {
+    public static void main(String[] args) {
+        Java_basic javaBasic = new Java_basic();
+        javaBasic.escpSequance();
+        javaBasic.starSequance();
+    }
+
+    void escpSequance() {
+        System.out.println("escape \nsequence");
+        System.out.println("double \"Double\" sequence ");
+
+    }
+
+    void starSequance() {
+        System.out.println("# \n# #\n# # #\n# # # #\n# # # # #\n# # # # # #\n# # # # # # #");
+    }
+}
