478f52746d5f945a0aef47ee156ffb6bb9a7e62d
pravinv1998 <pravin.wargad1998@gmail.com>
2025-11-18 22:37:11 +0530

    telusco course

--- Telusko/TeluskoJava.java
+++ Telusko/TeluskoJava.java
@@ -2,6 +2,10 @@ package Telusko;
 
 public class TeluskoJava {
     public static void main(String[] args) {
+        float f = 1.5f;
 
+        boolean bb = true;
+
+        System.out.println(bb);
     }
 }
