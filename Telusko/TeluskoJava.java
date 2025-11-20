608e1f2a64927bbf0d1d9835dbe9c88c089fef70
pravinv1998 <pravin.wargad1998@gmail.com>
2025-11-20 22:06:14 +0530

    20 Nov Commit

--- Telusko/TeluskoJava.java
+++ Telusko/TeluskoJava.java
@@ -2,10 +2,10 @@ package Telusko;
 
 public class TeluskoJava {
     public static void main(String[] args) {
-        float f = 1.5f;
+        int num1 = 10;
+        int num2 = 20;
 
-        boolean bb = true;
-
-        System.out.println(bb);
+        System.out.println(num1 + num2);
+        System.out.println(num1 + "" + num2);
     }
 }
