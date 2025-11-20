256261c6b374c39407a7bb20aec710c1ca6a83ab
pravinv1998 <pravin.wargad1998@gmail.com>
2025-11-20 22:21:31 +0530

    type_variable_literals

--- TeluskoJava.java
+++ TeluskoJava.java
@@ -1,8 +1,7 @@
-package Telusko;
 
 public class TeluskoJava {
     public static void main(String[] args) {
-        int num1 = 10;
+        int num1 = 10; // int-type, num1-variable, 10-literals
         int num2 = 20;
 
         System.out.println(num1 + num2);
